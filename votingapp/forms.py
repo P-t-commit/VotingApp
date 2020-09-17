f77b533c9612d3d879b0005ff6751b9a86512373
Pranav Taneja <69567193+P-t-commit@users.noreply.github.com>
2020-09-17 09:05:08 +0530

    With loginradius implementation

--- votingapp/forms.py
+++ votingapp/forms.py
@@ -5,16 +5,16 @@ from votingapp.models import User
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max = 20)])
+    #username = StringField('Username', validators=[DataRequired(), Length(min=2, max = 20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That username is already taken. Please choose a different one.')
+    # def validate_username(self, username):
+    #     user = User.query.filter_by(username=username.data).first()
+    #     if user:
+    #         raise ValidationError('That username is already taken. Please choose a different one.')
     
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
